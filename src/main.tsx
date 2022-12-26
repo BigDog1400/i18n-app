f39f813b59aa655c147a07581cb224699f375a19
BigDog1400 <const.isaac@gmail.com>
2022-12-26 11:37:57 -0400

    feat: add react-intl to dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { IntlProvider } from "react-intl";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <IntlProvider locale="en" defaultLocale='en'>
     <App />
+    </IntlProvider>
   </React.StrictMode>,
 )
