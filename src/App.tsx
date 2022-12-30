f2f04b5c402468d7509d302a5ad7a17ddfeb0d1a
BigDog1400 <const.isaac@gmail.com>
2022-12-30 08:31:33 -0400

    feat: add button to update lang

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import React, { useState } from 'react';
-import reactLogo from './assets/react.svg';
+import React from 'react';
 import './App.css';
 import { FormattedMessage } from 'react-intl';
 
 function App() {
+  const changeLanguage = (locale: string) => {
+    localStorage.setItem('locale', locale);
+    window.location.reload();
+  };
   return (
     <div>
       <FormattedMessage
@@ -11,6 +14,18 @@ function App() {
         defaultMessage="Como manejar i18n para aplicaciones React"
         description="Titulo de la aplicacion"
       />
+
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          gap: '10px',
+          padding: '10px',
+        }}
+      >
+        <button onClick={() => changeLanguage('es')}>ES</button>
+        <button onClick={() => changeLanguage('en')}>EN</button>
+      </div>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import { IntlProvider } from "react-intl";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { IntlProvider } from 'react-intl';
+import enMessages from './translations/compiled-messages/en.json';
+import esMessages from './translations/compiled-messages/es.json';
+
+const translations = {
+  en: enMessages,
+  es: esMessages,
+};
+
+const getUserLocale = () => {
+  const locale = localStorage.getItem('locale');
+  if ((locale && locale === 'es') || locale === 'en') {
+    return locale;
+  }
+  return 'en';
+};
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <IntlProvider locale="en" defaultLocale='en'>
-    <App />
+    <IntlProvider defaultLocale="en" locale={getUserLocale()} messages={translations[getUserLocale()]}>
+      <App />
     </IntlProvider>
   </React.StrictMode>,
-)
+);
